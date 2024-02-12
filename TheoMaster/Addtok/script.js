dc788f2a54c8ab4c52efc23895fca6bcf407e28a
TheoGits <theokrist07@gmail.com>
2024-02-12 22:39:19 +0100

    1.0
    Ideen kommer frem, men masse jobb både front end og backend. startet litt på javascripten.

---
+++ TheoMaster/Addtok/script.js
@@ -0,0 +1,13 @@
+let HD1 = document.getElementById ('HD1')
+let BS1 = document.getElementById ('BS1')
+
+let Title = ('Pen lampe')
+
+let Description = ('denne øker søvnen din med over 20% og dette kan føre til god konsetrasjon i skoletimer')
+
+function innlegg(){
+    HD1.textContent = Title
+    BS1.textContent = Description
+
+
+}
